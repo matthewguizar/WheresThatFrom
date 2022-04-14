435544e67a869211b0a4458e7c0d432d4fe560b0
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-14 11:22:48 -0700

    ayy

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 
 app.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, '..public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.get('/api/quote', quoteOfTheDay)
