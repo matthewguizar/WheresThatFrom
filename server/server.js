41dd6179baf5df0f6e16775c6632c2db7561e8c8
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-14 10:56:16 -0700

    trouble shooting

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors())
 
 
 app.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 app.get('/api/quote', quoteOfTheDay)
